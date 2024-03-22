58cfbd4c0fedcfb638d99c286f8844495f578a63
Suiorkul <ajdanbekov4>
2024-03-22 11:01:00 +0600

    add item

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-let box = document.querySelector(".box")
+let item = document.querySelector(".item")
 
+item.innerHTML = hello
 
-box.innerHTML = "s"
-
-let box1 = document.querySelector(".box1")
-
-
-box1.innerHTML = "s"
-
-let box2 = document.querySelector(".box2")
-
-
-box2.innerHTML = "s"
-
-let box3 = document.querySelector(".box3")
-
-
-box3.innerHTML = "s"
-
-let box4 = document.querySelector(".box4")
-
-
-box4.innerHTML = "s"
-
-let box5 = document.querySelector(".box5")
-
-
-box5.innerHTML = "s"
-
-let box6 = document.querySelector(".box6")
-
-
-box6.innerHTML = "S"
-
-let box7 = document.querySelector(".box7")
-
-
-box7.innerHTML = "S"
+let hello = "Bishkek"
